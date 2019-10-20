098ba02260786c41e763ed496ccb670d4fcae3e6
Brittany Dockter <bwitdoc@gmail.com>
2019-10-20 12:40:27 -0700

    adding week 1 files

---
+++ GitHub Files/Dockter-header-test.js
@@ -0,0 +1,17 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Brittany Dockter 
+; Date:   09 June 2019
+; Modified by: Brittany Dockter
+; Description: Displays a formatted header
+;===========================================
+*/
+
+// Begin Program
+
+var header = require('./Dockter-header.js');
+
+console.log(header.display("Brittany", "Dockter", "Exercise 2.2"));
+
+// End Program
