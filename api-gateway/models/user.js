91d3160cbb4b6a664865780a8dbb79ef1a8ace5e
Brittany Dockter <bwitdoc@gmail.com>
2019-11-09 14:33:10 -0800

    adding week 4 assignments

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -6,3 +6,12 @@ var userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
+
+module.exports.add = (user, callback) => {
+    user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query, callback);
+};
