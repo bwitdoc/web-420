86c15b8fff3f753ab8ecbd029277340ea4b9ea86
Brittany Dockter <bwitdoc@gmail.com>
2019-11-24 09:25:01 -0800

    updating files week 6

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -4,8 +4,10 @@ var auth_controller = require('../controllers/authController');
 
 // POST request for registering a user
 router.post('/auth/register', auth_controller.user_register);
+router.post('/auth/login', auth_controller.user_login);
 
 // GET request for verifying user tokens
 router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/logout', auth_controller.user_logout);
 
 module.exports = router;
