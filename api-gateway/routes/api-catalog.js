c385807ceb543c3d64e0a3f2441f6dba43a13ecc
Brittany Dockter <bwitdoc@gmail.com>
2019-12-07 09:31:47 -0800

    updating files

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -1,6 +1,7 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 var auth_controller = require('../controllers/authController');
+var checkToken = require('../check-token');
 
 // POST request for registering a user
 router.post('/auth/register', auth_controller.user_register);
@@ -9,5 +10,6 @@ router.post('/auth/login', auth_controller.user_login);
 // GET request for verifying user tokens
 router.get('/auth/token', auth_controller.user_token);
 router.get('/auth/logout', auth_controller.user_logout);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 
 module.exports = router;
