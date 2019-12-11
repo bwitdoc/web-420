45218603a7e396cb42ba124711fc6f9bba9e48ca
Brittany Dockter <bwitdoc@gmail.com>
2019-12-10 19:13:01 -0800

    updating files

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -3,18 +3,10 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
 
-exports.user_register = function(req, res) {
-    res.send("NOT IMPLEMENTED: User registration POST");
-};
-
-// Verify token on GET
-exports.user_token = function(req, res) {
-    res.send('NOT IMPLEMENTED: User token lookup GET');
-};
-
 // Register new user on POST
 exports.user_register = function(req, res) {
     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
     var newUser = new User({
         username: req.body.username,
         password: hashedPassword,
@@ -35,10 +27,11 @@ exports.user_register = function(req, res) {
 
 // Verify token on GET
 exports.user_token = function(req, res) {
-    User.getById(req.userId, function(err, user) {
-        if (err) return res.status(500).send('There was a problem finding the user.');
+    console.log(req.userId)
+        User.getById(req.userId, function(err, user) {
+            if (err) return res.status(500).send('There was a problem finding the user.');
 
-        if (!user) return res.status(404).send('No user found.');
+            if (!user) return res.status(404).send('No user found.');
 
         res.status(200).send(user);
     });
